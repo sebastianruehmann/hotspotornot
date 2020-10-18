10a317b50e43558b93efd35ccf39c26ef01cea28
Sebastian Rühmann <mail@sebastianruehmann.de>
2020-10-18 21:30:20 +0200

    Fix input style appearance

--- components/Input.js
+++ components/Input.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Input = styled.input`
+  appearance: none;
   padding: 16px;
   border-top-left-radius: 8px;
   border-bottom-left-radius: 8px;
