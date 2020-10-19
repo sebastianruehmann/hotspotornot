58363145216498949abd341a29ee4fccc486cb28
Sebastian Rühmann <mail@sebastianruehmann.de>
2020-10-19 22:18:24 +0200

    Use openstreetmaps without limitations

--- services/Geocoding.js
+++ services/Geocoding.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
 export const getCoordinatesByAddress = (address) => {
-  const url = `https://geocode.xyz/${address}?json=1&region=DE`
+  const url = `https://nominatim.openstreetmap.org/search?q=${address}&format=json&countrycodes=DE`
 
   return axios.get(url).then((response) => {
     return {
-      lng: response.data.longt,
-      lat: response.data.latt,
+      lng: response.data[0].lon,
+      lat: response.data[0].lat,
     }
   })
 }
