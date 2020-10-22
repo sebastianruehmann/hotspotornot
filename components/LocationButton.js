00bf94035d7d0b57ee99a57f95256a356a5e1bbb
Jonas Reitmann <jonas@reitmann.me>
2020-10-22 22:17:59 +0200

    Fix url

--- components/LocationButton.js
+++ components/LocationButton.js
@@ -18,7 +18,7 @@ export const LocationButton = () => {
             pathname: '/result',
             query: { coords },
           },
-          '/results'
+          '/result'
         )
         .then(() => window.scrollTo(0, 0))
     }
