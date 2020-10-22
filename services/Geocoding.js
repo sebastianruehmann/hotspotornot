63ada6f71d931f0bbfdae2318e0681506abe603c
Jonas Reitmann <jonas@reitmann.me>
2020-10-22 22:20:07 +0200

    Remove console.log

--- services/Geocoding.js
+++ services/Geocoding.js
@@ -4,7 +4,6 @@ export const getCoordinatesByAddress = (address) => {
   const url = `https://nominatim.openstreetmap.org/search?q=${address}&format=json&countrycodes=DE`
 
   return axios.get(url).then((response) => {
-    console.log(!response.data.length)
     if (!response.data.length) {
       throw new Error('No location data found')
     }
