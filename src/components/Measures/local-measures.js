127c91bc6186a1594888c53fc2a22b85d24afb3b
Jonas Reitmann <jonas@reitmann.me>
2020-10-26 14:12:02 +0100

    Change link text

--- src/components/Measures/local-measures.js
+++ src/components/Measures/local-measures.js
@@ -1,6 +1,6 @@
 export const localMeasures = {
   Hamburg: {
-    title: 'FAQ hamburg.de',
+    title: 'Offizielle Regeln auf hamburg.de',
     url: 'https://www.hamburg.de/coronavirus/13757524/das-ist-erlaubt/',
   },
 }
